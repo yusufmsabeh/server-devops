18d4ea073e648dda549469ffab00bc6c653c0f0f
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-07 18:10:20 +0300

    fix: remove unused variables

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   res.status(200).end("hello");
 });
 
-app.get("/hello", (req, res, next) => {
+app.get("/hello", (req, res) => {
   res.status(200).end("hello to");
 });
 app.listen(3000, () => {
