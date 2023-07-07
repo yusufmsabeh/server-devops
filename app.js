a256391dd4d241404c887913c942aac406692b3d
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-07 16:44:36 +0300

    feat: handle get / endpoint

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.status(200).end("hello");
+});
+
+app.listen(3000, () => {
+  console.log("server now is running on port 3000");
+});
