7d2cf7ad62d1e9e0dde55580354f27eeb9998770
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-07-07 18:08:48 +0300

    feat: handle get /hello endpoint

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.status(200).end("hello");
 });
 
+app.get("/hello", (req, res, next) => {
+  res.status(200).end("hello to");
+});
 app.listen(3000, () => {
   console.log("server now is running on port 3000");
 });
