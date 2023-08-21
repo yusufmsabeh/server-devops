a6cb3dc75092df2c2c5df9036b2ec4aa6aea4052
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-08-21 09:45:40 +0300

    feat: edit semantic release

--- release.config.js
+++ release.config.js
@@ -3,14 +3,13 @@ const config = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["*"],
-        message: `chore(release): ${nextReleas.version} [skip ci] \n\n${nextReleas.notes}`,
-      },
-    ],
-    "@semantic-release/github",
-  ],
+    "@semantic-release/npm",
+    ["@semantic-release/git", {
+      "assets": ["package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+    "@semantic-release/github"
+  ]
 };
+
 module.exports = config;
