312c7fb8dba31cded8734bcbca222a0b4979c6a5
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-08-21 11:09:55 +0300

    feat: change from /endpoints to /v1/endpoints

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/v1", (req, res) => {
   res.status(200).end("hello");
 });
 
-app.get("/hello", (req, res) => {
+app.get("/v1/hello", (req, res) => {
   res.status(200).end("hello User");
 });
 
-app.get("/time", (req, res) => {
+app.get("/v1/time", (req, res) => {
   const now = new Date();
   res.status(200).end("hello User the time is " + now);
 });
