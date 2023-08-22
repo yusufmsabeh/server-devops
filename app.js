c12db0d6e98e222a1f1fe30f22c322b28344056a
yusufmsabeh <yusuf.msabeh@gmail.com>
2023-08-22 15:52:32 +0300

    feat: change api v2 => v3

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from "express";
 const app = express();
 
-app.get("/v2", (req, res) => {
+app.get("/v3", (req, res) => {
   res.status(200).end("hello");
 });
 
-app.get("/v2/hello", (req, res) => {
+app.get("/v3/hello", (req, res) => {
   res.status(200).end("hello User");
 });
 
-app.get("/v2/time", (req, res) => {
+app.get("/v3/time", (req, res) => {
   const now = new Date();
   res.status(200).end("hello User the time is " + now);
 });
